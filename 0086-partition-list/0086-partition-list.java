bacd384e8f1d3f9fd82c8ae5a416d046533be885
Sanjana Mall <mallsanjana1912@gmail.com>
2026-09-07 22:58:19 +0530

    Time: 0 ms (100%), Space: 44 MB (13.83%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        // Dummy nodes for the two partitions
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+
+        // Traverse the original list
+        while (head != null) {
+
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+
+            head = head.next;
+        }
+
+        // Connect the two partitions
+        less.next = greaterDummy.next;
+
+        // Important: terminate the final list
+        greater.next = null;
+
+        return lessDummy.next;
+    }
+}
