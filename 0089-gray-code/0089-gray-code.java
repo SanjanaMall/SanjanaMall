7cf587cf27ae810717590e0f6a46c4482448417b
Sanjana Mall <mallsanjana1912@gmail.com>
2026-09-10 15:43:25 +0530

    Time: 5 ms (53.93%), Space: 53.5 MB (76.45%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        
+        List<Integer> result = new ArrayList<>();
+
+        int size = 1 << n;
+
+        for(int i = 0; i < size; i++){
+            int gray = i ^ (i >> 1);
+            result.add(gray);
+        }
+
+        return result;
+    }
+}
